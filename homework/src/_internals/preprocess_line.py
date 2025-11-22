16424d4fe1b853898aec7b27d79b017883a7550d
Cristian Camilo Florez Henao <cflorezh@unal.edu.co>
2025-11-22 09:40:36 -0500

    inernals2

---
+++ homework/src/_internals/preprocess_line.py
@@ -0,0 +1,3 @@
+def preprocess_line(all_lines):
+    all_lines = [line.strip().lower() for line in all_lines]
+    return all_lines
--- homework/src/_internals/read_all_lines.py
+++ homework/src/_internals/read_all_lines.py
@@ -4,9 +4,9 @@ import os
 def read_all_lines():
     all_lines = []
     input_files_list = os.listdir("data/input/")
-
     for filename in input_files_list:
-        with open(filename, "r", encoding="utf-8") as f:
+        file_path = os.path.join("data/input", filename)
+        with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
             all_lines.extend(lines)
     return all_lines
